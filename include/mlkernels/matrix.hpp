64fe0de7e69623965570a57749dd46310f657ea0
YusufBroujerdi <yusufbroujerdi@outlook.com>
2026-05-21 18:36:41 +0100

    Implements a matrix class to use for subsequent GEMM implementations. Establishes a coding convention.

--- include/mlkernels/matrix.hpp
+++ include/mlkernels/matrix.hpp
@@ -1,1 +1,62 @@
+#pragma once
 
+#include <vector>
+#include <cstddef>
+#include <algorithm>
+
+namespace mlk {
+
+template <typename T>
+class Matrix {
+
+public:
+
+    Matrix()
+        : rows_(0), cols_(0), data_() {}
+
+    Matrix(std::size_t rows, std::size_t cols)
+        : rows_(rows), cols_(cols), data_(rows * cols) {}
+
+    std::size_t rows() const {
+        return rows_;
+    }
+
+    std::size_t cols() const {
+        return cols_;
+    }
+
+    std::size_t size() const {
+        return data_.size();
+    }
+
+    const T* data() const {
+        return data_.data();
+    }
+
+    T* data() {
+        return data_.data();
+    }
+
+    const T& operator()(std::size_t row, std::size_t col) const
+    {
+        return data_[row * cols_ + col];
+    }
+
+    T& operator()(std::size_t row, std::size_t col)
+    {
+        return data_[row * cols_ + col];
+    }
+
+    void fill(T value)
+    {
+        std::fill(data_.begin(), data_.end(), value);
+    }
+
+private:
+
+    std::size_t rows_;
+    std::size_t cols_;
+    std::vector<T> data_;
+};
+
+}
